b87df267f439104ee7e9dfe7c28f7f3efc3b6d80
Carofort <dayannafort@gmail.com>
2026-07-23 09:06:40 -0500

    R3

--- src/main/java/ec/edu/ups/icc/labevaluation/laboratories/controllers/LaboratoryController.java
+++ src/main/java/ec/edu/ups/icc/labevaluation/laboratories/controllers/LaboratoryController.java
@@ -8,11 +8,13 @@ import ec.edu.ups.icc.labevaluation.laboratories.services.LaboratoryService;
 public class LaboratoryController {
     private final LaboratoryService service;
     public LaboratoryController(LaboratoryService service){this.service=service;}
+    
     @GetMapping("/available")
     @PreAuthorize("isAuthenticated()")
     public List<LaboratoryResponseDto> findAvailable(@RequestParam Long campusId, @RequestParam Integer minCapacity){
         return service.findAvailable(campusId, minCapacity);
     }
+
     @GetMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
     public LaboratoryResponseDto findOne(@PathVariable Long id){return service.findOne(id);}
